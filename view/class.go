1abe4d7b099d5ba8426592609eeb8f8da03e11a5
Adler <nkj20932@hotmail.com>
2017-05-10 23:57:27 +1000

    add basic HTML templates

--- view/class.go
+++ view/class.go
@@ -3,8 +3,11 @@ package view
 type Classes []Class
 
 type Class struct {
-	Methods Methods `json:"methods"`
-	Name    string  `json:"name"`
-	Line    int     `json:"line"`
-	Comment string  `json:"comment"`
+	Methods  Methods `json:"methods"`
+	Name     string  `json:"name"`
+	Line     int     `json:"line"`
+	Comment  string  `json:"comment"`
+	Filename string
+	Commit   string
+	Repo     string
 }
--- view/view.go
+++ view/view.go
@@ -2,8 +2,11 @@ package view
 
 import (
 	"encoding/json"
-	// "fmt"
+	"fmt"
+	"html/template"
 	"io/ioutil"
+	"os"
+	"strings"
 )
 
 func ReadFrom(filepath string) Classes {
@@ -17,10 +20,50 @@ func ReadFrom(filepath string) Classes {
 	if err != nil {
 		panic(err)
 	}
+	for i, class := range classes {
+		name := strings.ToLower(class.Name)
+		class.Filename = name
+		class.Repo = "https://github.com/goby-lang/goby"
+		class.Commit = "f32c1fcbfd7e1df021948de1065d342e95ebd03d"
+		classes[i] = class
+	}
 
 	return classes
 }
 
-func Generate(classes Classes) {
+func panicIf(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
 
+func generateIndexFile(classes Classes) {
+	indexSource, err := ioutil.ReadFile("./view/templates/index.html")
+	panicIf(err)
+	indexFile, err := os.OpenFile("./docs/index.html", os.O_CREATE|os.O_WRONLY, 0777)
+	panicIf(err)
+	indexTemplate, err := template.New("index").Parse(string(indexSource))
+	panicIf(err)
+	err = indexTemplate.Execute(indexFile, classes)
+	panicIf(err)
+	fmt.Println("Generated: ./docs/index.html")
+}
+
+func generateClassFile(class Class) {
+	classSource, err := ioutil.ReadFile("./view/templates/class.html")
+	panicIf(err)
+	classFile, err := os.OpenFile("./docs/"+class.Filename+".html", os.O_CREATE|os.O_WRONLY, 0777)
+	panicIf(err)
+	classTemplate, err := template.New(class.Filename).Parse(string(classSource))
+	panicIf(err)
+	err = classTemplate.Execute(classFile, class)
+	panicIf(err)
+	fmt.Println("Generated: ./docs/" + class.Name + ".html")
+}
+
+func Generate(classes Classes) {
+	generateIndexFile(classes)
+	for _, class := range classes {
+		generateClassFile(class)
+	}
 }
